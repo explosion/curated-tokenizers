d21e278739af29ea07da28351eb896a9748e06df
Daniël de Kok <me@danieldk.eu>
2022-07-09 22:04:54 +0200

    Build system fixes

--- setup.py
+++ setup.py
@@ -1,43 +1,9 @@
 #!/usr/bin/env python
-from __future__ import print_function
-import os
-import sys
-
-from setuptools import Extension, find_packages
-from Cython.Build import cythonize
 
 from skbuild import setup
 
-PACKAGES = find_packages()
-MOD_NAMES = ["cysp.sp"]
-
-
-def clean(path):
-    for name in MOD_NAMES:
-        name = name.replace(".", "/")
-        for ext in [".so", ".html", ".cpp", ".c"]:
-            file_path = os.path.join(path, name + ext)
-            if os.path.exists(file_path):
-                os.unlink(file_path)
-
-
-def setup_package():
-    extensions = [
-        Extension(
-            "cysp.sp",
-            ["cysp/sp.pyx"],
-            language="c++",
-        ),
-    ]
-
-    setup(
-        name="cysp",
-        zip_safe=True,
-        packages=PACKAGES,
-        package_data={"": ["*.pyx", "*.pxd"]},
-        ext_modules=cythonize(extensions),
-    )
-
-
-if __name__ == "__main__":
-    setup_package()
+setup(
+    name="cysp",
+    version="0.0.1",
+    packages=["cysp"],
+)
