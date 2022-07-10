a2370936083be61746272c59b1c0bc644ac8b2cd
Daniël de Kok <me@danieldk.eu>
2022-07-10 17:39:13 +0200

    Basic initial encode method

---
+++ cysp/test/test_sp.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+import pytest
+
+from cysp.sp import Processor
+
+
+@pytest.fixture(scope="module")
+def test_dir(request):
+    return Path(request.fspath).parent
+
+
+def test_load_unknown_file():
+    with pytest.raises(OSError, match=r"No such file"):
+        Processor.load_file("bogus.model")
+
+
+def test_toy_model(test_dir):
+    spp = Processor.load_file(str(test_dir / "toy.model"))
+    ids, pieces = spp.encode("I saw a girl with a telescope.")
+    print(pieces)
+    assert ids == [8, 465, 10, 947, 41, 10, 170, 168, 110, 28, 20, 143, 4]
+    assert pieces == [
+        "▁I",
+        "▁saw",
+        "▁a",
+        "▁girl",
+        "▁with",
+        "▁a",
+        "▁t",
+        "el",
+        "es",
+        "c",
+        "o",
+        "pe",
+        ".",
+    ]
