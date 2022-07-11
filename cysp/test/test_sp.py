72036517dc0d08cf208d507a86027faace592578
Daniël de Kok <me@danieldk.eu>
2022-07-11 11:02:33 +0200

    Test input with NUL character

--- cysp/test/test_sp.py
+++ cysp/test/test_sp.py
@@ -10,16 +10,27 @@ def test_dir(request):
     return Path(request.fspath).parent
 
 
+@pytest.fixture
+def toy_model(test_dir):
+    return Processor.load_file(str(test_dir / "toy.model"))
+
+
 def test_load_unknown_file():
     with pytest.raises(OSError, match=r"No such file"):
         Processor.load_file("bogus.model")
 
 
-def test_toy_model(test_dir):
-    spp = Processor.load_file(str(test_dir / "toy.model"))
-    ids, pieces = spp.encode("I saw a girl with a telescope.")
-    print(pieces)
-    numpy.testing.assert_equal(ids, [8, 465, 10, 947, 41, 10, 170, 168, 110, 28, 20, 143, 4])
+def test_handles_nul_character(toy_model):
+    ids, pieces = toy_model.encode("Test\0 nul")
+    numpy.testing.assert_equal(ids, [239, 382, 0, 7, 24, 231])
+    assert pieces == ["▁T", "est", "\0", "▁", "n", "ul"]
+
+
+def test_toy_model(toy_model):
+    ids, pieces = toy_model.encode("I saw a girl with a telescope.")
+    numpy.testing.assert_equal(
+        ids, [8, 465, 10, 947, 41, 10, 170, 168, 110, 28, 20, 143, 4]
+    )
     assert pieces == [
         "▁I",
         "▁saw",
