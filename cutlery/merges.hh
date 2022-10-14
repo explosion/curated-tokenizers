dce050f4f755b25d40badcefcb6a52e7d9e6f242
Daniël de Kok <me@danieldk.eu>
2022-10-14 10:51:08 +0200

    Add reference to hash_combine docs

--- cutlery/merges.hh
+++ cutlery/merges.hh
@@ -7,7 +7,9 @@
 
 typedef std::pair<std::string, std::string> string_pair;
 
-// Hash combine function from Boost.
+// Hash combine function from Boost, see
+// https://www.boost.org/doc/libs/1_55_0/doc/html/hash/combine.html
+// for usage information.
 template <class T>
 inline void hash_combine(std::size_t& seed, const T& v)
 {
